2a997b21e6fa7e487297b3a4769cd8e6d0347d7a
Wouter Admiraal <wad@wadmiraal.net>
2015-09-24 11:47:01 +0200

    Add dummy test for the Day model.

---
+++ test/specs/model/day.js
@@ -0,0 +1,22 @@
+/**
+ * @file
+ * Book model specs.
+ *
+ * @author Wouter Admiraal <wad@wadmiraal.net>
+ * @license MIT
+ */
+
+define([], function() {
+    'use strict';
+
+    describe( 'Day model', function() {
+
+        describe( 'when loaded by RequireJS', function() {
+            it( 'should exist', function() {
+                var DayModel = 1;
+                expect( DayModel ).toBeDefined();
+            });
+        });
+    });
+});
+
