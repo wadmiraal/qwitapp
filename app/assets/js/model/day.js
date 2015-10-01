800afa2557e272aeff8a7dcb6a7d6646eb119b51
Wouter Admiraal <wad@wadmiraal.net>
2015-10-01 09:57:45 +0200

    Update the DayModel spec. Add a better description of what it is for.

--- app/assets/js/model/day.js
+++ app/assets/js/model/day.js
@@ -2,7 +2,9 @@
  * @file
  * Day model.
  *
- * Data and information about one day.
+ * Data and information about one day. A day has a date and a "good" attribute.
+ * If the day was "good", the good attribute will equal true. Otherwise, it
+ * equals false. If a day was good, the user did not succumb to his temptation.
  *
  * @author Wouter Admiraal <wad@wadmiraal.net>
  * @license MIT
--- test/specs/model/day.js
+++ test/specs/model/day.js
@@ -6,17 +6,26 @@
  * @license MIT
  */
 
-define([], function() {
+define([ 'app/model/day' ], function( DayModel ) {
     'use strict';
 
     describe( 'Day model', function() {
 
         describe( 'when loaded by RequireJS', function() {
             it( 'should exist', function() {
-                var DayModel = 1;
                 expect( DayModel ).toBeDefined();
+            });
+        });
+
+        describe( 'when instantiated', function() {
+            it( 'should exhibit attributes', function() {
+                var day = new DayModel({
+                    good: true,
+                    date: '2015-09-20'
+                });
+                expect( day.get( 'good' ) ).toEqual( true );
+                expect( day.get( 'date' ) ).toEqual( '2015-09-20' );
             });
         });
     });
 });
-
