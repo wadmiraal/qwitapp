00038ea02e621991af9c758f89f24ed38530f59b
Wouter Admiraal <wad@wadmiraal.net>
2015-10-01 10:26:47 +0200

    Update collection logic, add specs.

--- app/assets/js/collection/days.js
+++ app/assets/js/collection/days.js
@@ -2,18 +2,39 @@
  * @file
  * Day collection.
  *
- * Loading of one or more days.
+ * Collection of one or more DayModels.
  *
  * @author Wouter Admiraal <wad@wadmiraal.net>
  * @license MIT
  */
 
-define([ 'backbone', 'app/model/day' ], function( Backbone, DayModel ) {
+define([ 'backbone', 'localstorage', 'app/model/day' ], function( Backbone, a, DayModel ) {
     'use strict';
 
     var DayCollection = Backbone.Collection.extend({
         model: DayModel,
-        url: '/days'
+        localStorage: new Backbone.LocalStorage( 'qwit-days' ),
+        totalGood: function() {
+            return this.where({ good: true }).length;
+        },
+        totalBad: function() {
+            return this.where({ good: false }).length;
+        },
+        longestStreak: function() {
+            var count = 0,
+                max = 0;
+
+            this.each( function( day ) {
+                if ( day.get( 'good' ) ) {
+                    count++;
+                } else {
+                    max = Math.max( max, count );
+                    count = 0;
+                }
+            });
+
+            return max;
+        }
     });
 
     return DayCollection;
