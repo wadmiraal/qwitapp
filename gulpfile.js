52b416df54dfb96049d4d61cbcd1fb1fa4b689ba
Wouter Admiraal <wad@wadmiraal.net>
2016-05-01 19:27:25 +0200

    Revert "Change build settings (again)."
    This reverts commit 949049d668ffa684a56c16a2e846ad7c2f7887f2.

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,8 @@ var gulp       = require( 'gulp' ),
     concat     = require( 'gulp-concat' ),
     minifyCSS  = require( 'gulp-minify-css' ),
     uglify     = require( 'gulp-uglify' ),
-    minifyHTML = require( 'gulp-minify-html' );
+    minifyHTML = require( 'gulp-minify-html' ),
+    shell      = require( 'gulp-shell' );
 
 // Compile the SCSS files using libSass.
 gulp.task( 'sass', function() {
@@ -28,15 +29,20 @@ gulp.task( 'css-min', function() {
   gulp.src([ './app/assets/css/**/*.css', './app/assets/css/*.css' ])
     .pipe( concat( 'all.min.css' ) )
     .pipe( minifyCSS() )
-    .pipe( gulp.dest( './build/css' ) );
+    .pipe( gulp.dest( './build' ) );
 });
+
+// Use r.js to combine all JS files.
+gulp.task( 'rjs', shell.task([
+  './node_modules/.bin/r.js -o build.js'
+]) );
 
 // Watch files for changes.
 gulp.task( 'watch', function() {
   gulp.watch( [ './app/assets/sass/*.scss', './app/assets/sass/**/*.scss' ], [ 'sass' ] );
+  gulp.watch( [ './app/assets/js/*.js', './app/assets/js/**/*.js' ], [ 'rjs' ] );
 });
 
-
 // Default tasks.
-gulp.task( 'default', [ 'sass', 'css-min' ] );
+gulp.task( 'default', [ 'sass', 'css-min', 'rjs' ] );
 
